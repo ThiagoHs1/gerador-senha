3e5d2db9680a2ef3b5e14ab10bc7f5da3312deef
Thiago Henrique Heinle Sanchez <thiagohenque@live.com>
2024-09-12 14:24:15 -0300

    iniciando o projeto

---
+++ src/main/java/thiagoh/App.java
@@ -0,0 +1,14 @@
+package thiagoh;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    public static void main(String[] args) {
+        GeradorSenhaGUI geradorSenhaGUI = new GeradorSenhaGUI();
+        geradorSenhaGUI.setVisible(true);
+    }
+}
